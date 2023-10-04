96b11723d5bfac2acd00484f3fb16d46b8ec2cb1
Joseph Mendoza <josephmenjunior@gmail.com>
2023-10-04 10:22:58 -0600

    Añadiendo ejercicio de práctica

---
+++ EjercicioDePrueba/Team.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EjercicioDePrueba;
+
+namespace EjercicioDePrueba
+{
+    public class Team<T>
+    {
+        public string CoachName { get; set; }
+        public string Nombre { get; set; }
+        public double Peso { get; set; }
+        public double Altura { get; set; }
+        public Team(string coachName, string nombre, double peso, double altura)
+        {
+            CoachName = coachName;
+            Nombre = nombre;
+            Peso = peso;
+            Altura = altura;
+        }
+
+
+    }
+}
