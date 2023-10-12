31b067706f01fbc78b3b6a8445d4c82574500501
Joseph Mendoza <josephmenjunior@gmail.com>
2023-10-12 10:19:01 -0600

    Explicacion y ejemplos de colecciones

---
+++ PruebaDictionary/Program.cs
@@ -0,0 +1,19 @@
+﻿using PruebaDictionary;
+
+Student std1 = new(1, "Pedro", "López", 18);
+Student std2 = new(2, "Susana", "Pérez", 20);
+Student std3 = new(3, "Roberto", "Zapata", 17);
+Student std4 = new(4, "Andrés", "Espinoza", 21);
+
+Dictionary<int, Student> students = 
+    new Dictionary<int, Student>();
+
+students.Add(std1.Id, std1);
+students.Add(std2.Id, std2);
+students.Add(std3.Id, std3);
+students.Add(std4.Id, std4);
+
+foreach (var item in students.Values)
+{
+    Console.WriteLine("Nombre: {0}; Edad: {1}", item.Name, item.Age); 
+}
---
+++ PruebaSortedList/Program.cs
@@ -0,0 +1,21 @@
+﻿SortedList<int, string> automoviles =
+    new SortedList<int, string>();
+
+automoviles.Add(754, "Honda");
+automoviles.Add(489, "Tesla");
+automoviles.Add(737, "Ford");
+automoviles.Add(256, "Toyota");
+
+foreach (var item in automoviles)
+{
+    Console.WriteLine("({0}, {1})", item.Key, item.Value);
+}
+
+Console.WriteLine("--------------------------------");
+
+Console.WriteLine(automoviles.Count);
+
+Console.WriteLine(automoviles.ContainsKey(489));
+Console.WriteLine(automoviles.ContainsValue("Tesla"));
+
+Console.WriteLine("--------------------------------");
