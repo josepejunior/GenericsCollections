9487b74c01ee25ae8b0ab92d368a76617a9b6672
Joseph Mendoza <josephmenjunior@gmail.com>
2023-10-25 10:01:41 -0600

    Prueba lambda y prueba LINQ

--- PruebaLambda/Program.cs
+++ PruebaLambda/Program.cs
@@ -33,20 +33,61 @@ if (cliente2 != null)
 else
     Console.WriteLine("Cliente no encontrado");
 
-//int BuscarCliente(Client c)
-//{
-//    return c.ID;
-//}
-
 Console.WriteLine("-----------------------");
 
 List<int> numeros = new List<int>();
-numeros.AddRange(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
+numeros.AddRange(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 17, 18, 18, 20, 21, 22, 23});
 
-// HAcemos uso de la expresion lambda
+// Hacemos uso de la expresion lambda para encontrar números pares
 List<int> numPares = numeros.FindAll(x => (x % 2) == 0);
 
 foreach (var item in numPares)
 {
     Console.WriteLine(item);
 }
+
+Console.WriteLine("-----------------------");
+
+// Hacemos uso de la expresion lambda para encontrar números pares
+List<int> numPrimos = numeros.FindAll(n =>
+{
+    if (n <= 1)
+        return false;
+    if (n <= 3)
+        return true;
+    if (n % 2 == 0 || n % 3 == 0)
+        return false;
+    return true;
+});
+
+foreach (var item in numPrimos)
+{
+    Console.Write("El numero primo es: ");
+    Console.WriteLine(item);
+}
+
+Console.WriteLine("-----------------------");
+
+numPares.ForEach(n =>
+{
+    Console.Write("El numero par es: ");
+    Console.WriteLine(n);
+});
+
+Client cliente4 = new Client();
+cliente4.ID = 4;
+cliente4.Name = "Pablo";
+
+Client cliente5 = new Client();
+cliente5.ID = 5;
+cliente5.Name = "Carlos";
+
+
+ComparaClientes compraNombre = (c1, c2) => 
+c1 == c2;
+
+Console.WriteLine("-----------------------");
+
+Console.WriteLine(compraNombre(cliente4.Name, cliente5.Name));
+
+delegate bool ComparaClientes(string n1, string n2);
---
+++ PruebaLinQ/Program.cs
@@ -0,0 +1,14 @@
+﻿// 1. Data source
+int[] numeros = Enumerable.Range(1, 100).ToArray();
+
+// 2. Query creation
+IEnumerable<int> numPares = 
+    from num in numeros
+    where num % 2 == 0
+    select num;
+
+// 3. Query execution
+foreach(var num in numPares)
+{
+    Console.Write("{0, 1} ", num);
+}
